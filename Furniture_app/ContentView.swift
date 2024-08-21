0d66c6f1ed1ac3b1462601cf43488c0208833bf3
Cinta jose <cjose@orthofx.com>
2024-08-21 17:23:50 +0530

    added appbar and tagline views

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -12,14 +12,9 @@ struct ContentView: View {
         ZStack {
             Color(.bg).ignoresSafeArea()
             
-            HStack {
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image("menu")
-                        .padding()
-                        .background(Color(.white))
-                        .cornerRadius(10)
-                })
-                
+            VStack(alignment: .leading){
+                AppBarView()
+                TagLineView()
             }
         }
         
@@ -29,5 +24,34 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image("menu")
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10)
+            })
+            Spacer()
+            Image("Profile")
+                .resizable()
+                .frame(width:42, height: 42)
+                .cornerRadius(10)
+        }.padding(.all)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        Text("Find The \nBest ")
+            .font(.custom("PlayfairDisplay-Regular", fixedSize: 28))
+            .foregroundColor(Color("Primary"))
+        + Text("Furniture!")
+            .font(.custom("PlayfairDisplay-Bold", fixedSize: 28))
+            .foregroundColor(Color("Primary"))
     }
 }
